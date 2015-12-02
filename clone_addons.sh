419b691c8fc57fbab48d97cb9cf028ebbc1e8f18
Joel Gethin Lewis <me@joelgethinlewis.com>
2015-12-02 17:35:12 +0000

    Added clone_addons.sh script to allow for quick porting to other computers

---
+++ clone_addons.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Clones dependencies
+# !Run this script from the project root!
+# In terminal cd to project root then "sh cloneaddons.sh"
+
+# switch to addons directory
+cd ../../addons
+
+# passing no arguments means read only
+if [ -z $1 ]; then
+CLONE="git clone https://github.com/"
+echo "Cloning read-only"
+else
+CLONE="git clone git@github.com:"
+echo "Cloning writable"
+fi
+
+# git clone specific revision of the addons
+# this ensures that things won't break if repositories are updated
+
+${CLONE}Bentleyj/ofxIntegrator
+# cd xxx
+# git checkout xxx
+# cd ..
+${CLONE}andreasmuller/ofxAutoReloadedShader
+${CLONE}kylemcdonald/ofxCv
+#cd ofxCv
+#git checkout develop
+#cd ..
+
+# final step is to cd back to the start dir
+cd ../apps/FourRings
